9014cf03eeeb65066be0fbc332af97328d59b9bc
Harris Suhayb <harrissuhayb@gmail.com>
2019-04-23 05:41:27 +0500

    adder complete

--- 0002/1.cpp
+++ 0002/1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
-
+// Program for adding two integers, sum range: -2147483648 to 2147483647
 int main(int argc, char const *argv[])
 {
     int a;
     int b;
-    int c; 
-    cout <<"enter a" << endl;
-    cin  >> a;
-    cout <<"enter b" << endl;
+    int c;
+    cout << "value adder by Harris..." << endl;
+    cout << "enter a : ";
+    cin >> a;
+    cout << "enter b : ";
     cin >> b;
     c = a + b;
-    cout << a << " " << "+" << " " << b << " " << "=" << " " << c <<" " << endl; 
+    cout << a << " + " << b << " = " << c << endl;
     return 0;
 }
