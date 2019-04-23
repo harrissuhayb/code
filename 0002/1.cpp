40b688fc48586ce40d0851e2742a7219a1898d31
Harris Suhayb <harrissuhayb@gmail.com>
2019-04-23 05:11:15 +0500

    fourth commit

--- 0002/1.cpp
+++ 0002/1.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a = 3;
-    int b = 4;
-    int c = 7; 
+    int a = 15;
+    int b = 9;
+    int c = a+b; 
     cout << a << "+" << b << "=" << c << endl; 
     return 0;
 }
