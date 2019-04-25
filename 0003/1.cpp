f0a9c163cadf99bcd7733543dc1a68e933a1807d
Harris Suhayb <harrissuhayb@gmail.com>
2019-04-25 07:44:03 +0500

    Improving Guessing Game

--- 0003/1.cpp
+++ 0003/1.cpp
@@ -7,8 +7,7 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL));
     cout << "Sum guessing game by Harris..." << endl;
-    int score = 0;
-    int wrong = 0;
+    int score = 0, wrong = 0, tries = 5;
     do
     {
 
@@ -21,19 +20,22 @@ int main(int argc, char const *argv[])
         int guess;
         cout << "Enter your guess: ";
         cin >> guess;
+
         if (guess == c)
+        { // right answer
             cout << "You are correct" << endl;
+            score = score + 1;
+        }
         else
+        { // wrong answer
             cout << "You are wrong, "
                  << "Answer is : " << c << endl;
-        if (guess == c)
-            score = score + 1;
-        else
             wrong = wrong + 1;
+        }
         cout << "Your score is : " << score << endl;
 
-    } while (wrong != 3);
+    } while (wrong != tries);
     cout << "You lost" << endl;
-    cout << "3 wrong tries, you have lost." << endl;
+    cout << wrong << " wrong tries, you have lost." << endl;
     return 0;
 }
