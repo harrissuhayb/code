fa43258bf349e50b2e9bf2ac845012ef78af4de3
Harris Suhayb <harrissuhayb@gmail.com>
2019-04-25 07:38:10 +0500

    Guessing Game

--- 0003/1.cpp
+++ 0003/1.cpp
@@ -6,12 +6,34 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
+    cout << "Sum guessing game by Harris..." << endl;
+    int score = 0;
+    int wrong = 0;
+    do
+    {
 
-    cout << "value adder by Harris..." << endl;
-    int a = rand() % 100;
-    int b = rand() % 100;
-    int c;
-    c = a + b;
-    cout << a << " + " << b << " = " << c << endl;
+        int a = rand() % 100;
+        int b = rand() % 100;
+        int c;
+        c = a + b;
+        cout << a << " + " << b << " = "
+             << "?" << endl;
+        int guess;
+        cout << "Enter your guess: ";
+        cin >> guess;
+        if (guess == c)
+            cout << "You are correct" << endl;
+        else
+            cout << "You are wrong, "
+                 << "Answer is : " << c << endl;
+        if (guess == c)
+            score = score + 1;
+        else
+            wrong = wrong + 1;
+        cout << "Your score is : " << score << endl;
+
+    } while (wrong != 3);
+    cout << "You lost" << endl;
+    cout << "3 wrong tries, you have lost." << endl;
     return 0;
 }
